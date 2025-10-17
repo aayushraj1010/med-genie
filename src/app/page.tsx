978341232273ffab2ed082e227908dc79a1478d0
aayushraj1010 <aayushraj1052@gmail.com>
2025-10-18 00:17:38 +0530

    Revert "feat: add TrustBadges component"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-import "./globals.css";
+import HomePage from "@/components/landing_page/HomePage";
+import './globals.css'
 import Navbar from "@/components/landing_page/NavBar";
 import Hero from "@/components/landing_page/HeroSection";
 import FeatureSection from "@/components/landing_page/Features";
@@ -12,42 +13,29 @@ import PresenceBoostGuide from "@/components/landing_page/PresenceBoost";
 import FAQs from "@/components/landing_page/FAQ";
 import Footer from "@/components/landing_page/Footer";
 import Preloader from "@/components/Preloader";
-import TrustBadges from "@/components/TrustBadges";
 
-export default function App() {
-  useEffect(() => {
-    try {
-      AOS.init({
-        duration: 800,
-        once: true,
-      });
-    } catch (err) {
-      // ignore AOS errors in environments where it isn't available
-      // eslint-disable-next-line no-console
-      console.warn('AOS init failed', err);
-    }
-
-    const t = setTimeout(() => {
-      try {
-        AOS.refreshHard();
-      } catch {
-        // ignore
-      }
+export default function App(){
+   useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    });
+    setTimeout(() => {
+      AOS.refreshHard();
     }, 800);
+  }, [])
 
-    return () => clearTimeout(t);
-  }, []);
-
-  return (
-    <>
-      <Preloader />
-      <Navbar />
-      <Hero />
-      <TrustBadges />
-      <FeatureSection />
-      <PresenceBoostGuide />
-      <FAQs />
-      <Footer />
-    </>
-  );
+  return (<>
+  <Preloader />
+  <main>
+   <Navbar/>
+    <Hero/>
+    <FeatureSection/>
+    <PresenceBoostGuide/>
+    <FAQs/>
+    <Footer/>
+    </main>
+  </>)
 }
+
+
--- src/types/jsx.d.ts
+++
@@ -1,7 +0,0 @@
-// Temporary JSX declaration to avoid editor/type errors when node_modules are not installed.
-// Remove this file once dependencies are installed and @types/react is available.
-declare namespace JSX {
-  interface IntrinsicElements {
-    [elemName: string]: any;
-  }
-}
